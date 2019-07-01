21743283b8963905f3f6110c3202c9576521ce81
shihongdong <imkobedroid@gmail.com>
2019-07-01 14:15:44 +0800

    自定义view的编写

--- app/src/main/java/com/remote/activity/weight/CustomView1.java
+++ app/src/main/java/com/remote/activity/weight/CustomView1.java
@@ -10,11 +10,20 @@ import android.util.Log;
 import android.view.View;
 import com.remote.R;
 
+/**
+ * Paint.setStyle(Style style) 设置绘制模式
+ * Paint.setColor(int color) 设置颜色
+ * Paint.setStrokeWidth(float width) 设置线条宽度
+ * Paint.setTextSize(float textSize) 设置文字大小
+ * Paint.setAntiAlias(boolean aa) 设置抗锯齿开关
+ */
+
 
 /**
  * @author Dsh  imkobedroid@gmail.com
  * @date 2019-06-28
  */
+
 public class CustomView1 extends View {
 
     Context context;
